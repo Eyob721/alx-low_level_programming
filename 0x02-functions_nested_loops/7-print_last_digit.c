51e84306c1c64dd782dc32736eda21868fc95902
eyob721 <eyobmelkamu721@gmail.com>
2023-03-18 06:49:51 +0300

    finished task - 0x02 task 7 (7-print_last_digit.c)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of an integer
+ * @x: an integer
+ *
+ * Return: the last digit of @x
+ */
+int print_last_digit(int x)
+{
+	int digit = x % 10;
+
+	if (digit < 0)
+		digit *= -1;
+
+	_putchar(digit + '0');
+
+	return (digit);
+}
+
