0ef032c54f5fdca3f284e6ac1c13ab9dfc08c83f
eyob721 <eyobmelkamu721@gmail.com>
2023-03-21 17:07:45 +0300

    finished task: 0x0D task 3 (3-function_like_macro.h)

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-1 * (x)) : (x))
+
+/* a dummy variable to make ISO C standard happy ;) */
+typedef int make_iso_compilers_happy;
+
+#endif
