59d2b9c600d1efc924e92c4342792c404a0c4c32
eyob721 <eyobmelkamu721@gmail.com>
2023-03-21 16:54:19 +0300

    fixed task: 0x0D task 1 (1-pi.h): ISO empty translation error

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,6 +2,8 @@
 #define _1_PI_H
 
 #define PI 3.14159265359
-typedef int __gmp_dummy_typedef;
+
+/* a dummy variable to make ISO C standard happy ;) */
+typedef int make_iso_compilers_happy;
 
 #endif
