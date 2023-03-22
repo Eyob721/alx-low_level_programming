183fe14b5a10c6e10689b6c89973af70a4cfd059
eyob721 <eyobmelkamu721@gmail.com>
2023-03-22 17:57:47 +0300

    finished task: 0x0F task 2 (2-int_index.c)

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer
+ * @array: an array of integers
+ * @size: size of the array
+ * @cmp: callback function used to compare elements of the array
+ *
+ * Return: index of the integer if the integer is found,
+ * -1 on failure or if integer is not found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	return (-1);
+}
