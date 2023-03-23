f4efaa6b46da0b72fbf8b618a3b5aee8410817e2
eyob721 <eyobmelkamu721@gmail.com>
2023-03-23 16:42:44 +0300

    finished task: 0x10 task 1 (1-print_numbers.c)

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	if (separator == NULL || n == 0)
+		return;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ptr, int), separator);
+	printf("%d\n", va_arg(ptr, int));
+
+	va_end(ptr);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -5,3 +5,7 @@
 - 0-sum_them_all.c
 
   - a function that returns the sum all its parameters
+
+- 1-print_numbers.c
+
+  - print_numbers - a function that prints numbers, followed by a new line
