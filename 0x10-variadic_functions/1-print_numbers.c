ec57d317885a9602251062ba588240868d0bf657
eyob721 <eyobmelkamu721@gmail.com>
2023-03-23 17:52:46 +0300

    fixed task: 0x10 task 1 (1-print_numbers.c)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 
+	if (n == 0)
+	{
+		va_end(ptr);
+		return;
+	}
+
 	va_start(ptr, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ptr, int));
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(ptr, int));
+	printf("\n");
 
 	va_end(ptr);
 }
