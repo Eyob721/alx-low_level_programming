fec5be5cb5c5f850b2b21be05f0194d4ee5cc843
eyob721 <eyobmelkamu721@gmail.com>
2023-03-30 19:47:31 +0300

    finished task: 0x0E task 0 (dog.h)

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,7 @@
 # 0x0E. C - Structures, typedef
+
+## Mandatory
+
+- dog.h
+
+  - a header file that contains a data structure to hold an information about a dog.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: this is a structure to hold some informations about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
