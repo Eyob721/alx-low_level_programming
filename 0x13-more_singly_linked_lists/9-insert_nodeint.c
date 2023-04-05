16d18c46e0d77313fb2bda4fab9abd9c6b96df2f
eyob721 <eyobmelkamu721@gmail.com>
2023-04-05 17:31:22 +0300

    optimized task 9 code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,15 +28,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = current;
 	if (current == *head)
-	{
-		new_node->next = *head;
 		*head = new_node;
-	}
 	else
-	{
-		new_node->next = current;
 		previous->next = new_node;
-	}
 	return (new_node);
 }
