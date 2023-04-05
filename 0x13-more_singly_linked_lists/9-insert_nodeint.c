2b1142e45e5ad83412470f5b9a91235270c1b0fa
eyob721 <eyobmelkamu721@gmail.com>
2023-04-05 13:54:59 +0300

    fixed task: 0x13 task 9 (9-insert_nodeint.c): for insertion at indexs 0 and 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,8 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-	previous = *head;
-	current = *head;
+	previous = current = *head;
 	for (i = 0; i < idx && current != NULL; i++)
 	{
 		previous = current;
