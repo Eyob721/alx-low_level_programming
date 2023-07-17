988850877112f50ea296891e86c3677c99633112
eyob721 <eyobmelkamu721@gmail.com>
2023-07-17 23:56:29 +0300

    Finish task 0: dog.h

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,11 @@
 # 0x0E. C - Structures, typedef
+
+## Mandatory
+
+dog.h
+
+- a header file that contanis a definition of a new type struct dog with the
+following elements:
+	- name, type = char *
+	- age, type = float
+	- owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This is a data structure to hold information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
