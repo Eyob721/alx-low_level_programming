be155a37bda586d2bf4af0e542ae2c72a03cdf81
eyob721 <eyobmelkamu721@gmail.com>
2023-07-17 11:54:45 +0300

    Optimize _strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-
+	if (s1 != NULL && s2 != NULL)
+		while (*s1 != '\0' || *s2 != '\0')
+		{
+			if (*s1 != *s2)
+				return (*s1 - *s2);
+			s1++, s2++;
+		}
 	return (0);
 }
