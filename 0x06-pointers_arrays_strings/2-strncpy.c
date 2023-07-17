d5e21d549ad0b025c98913a051a1f0b0116e3922
eyob721 <eyobmelkamu721@gmail.com>
2023-07-17 11:48:08 +0300

    Optimize _strncpy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
-		dest[i] = src[i];
-
-	for (; i < n; i++)
-		dest[i] = '\0';
-
+	if (dest != NULL && src != NULL)
+	{
+		for (i = 0; src[i] != '\0' && i < n; i++)
+			dest[i] = src[i];
+		while (i < n)
+			dest[i++] = '\0';
+	}
 	return (dest);
 }
