43072252ca22d5f6b61b66f403bb81fe626ccacf
eyob721 <eyobmelkamu721@gmail.com>
2023-07-17 10:12:12 +0300

    Optimize _strcpy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(src + i) != '\0'; i++)
-		*(dest + i) = *(src + i);
-	*(dest + i) = *(src + i);
-
+	if (dest != NULL && src != NULL)
+	{
+		while (*src != '\0')
+			dest[i++] = *(src++);
+		dest[i] = '\0';
+	}
 	return (dest);
 }
 
--- 0x05-pointers_arrays_strings/tests/9-main.c
+++ 0x05-pointers_arrays_strings/tests/9-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -9,10 +10,28 @@
 int main(void)
 {
 	char s1[98];
-	char *ptr;
+	char *got, *exp;
 
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
+	printf("Test 1\n-------------------------------------------------------\n");
+	got = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("[GOT]: %s", got);
+	exp = strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("[EXP]: %s", exp);
+
+	printf("Test 2\n-------------------------------------------------------\n");
+	got = _strcpy(s1, NULL);
+	printf("s1: %s\n", s1);
+	printf("[GOT]: %s\n", got);
+
+	/*strcpy can not take NULL */
+	/* exp = strcpy(s1, NULL); */
+	/* printf("%s", exp); */
+
+	printf("Test 2\n-------------------------------------------------------\n");
+	got = _strcpy(NULL, NULL);
+	printf("[GOT]: %s\n", got);
+	/* exp = strcpy(NULL, NULL); */
+	/* printf("%s", exp); */
+
 	return (0);
 }
