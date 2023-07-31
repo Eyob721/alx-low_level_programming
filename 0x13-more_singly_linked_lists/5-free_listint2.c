065bac82f72388b5f6a69116ceadb890eaf402c8
eyob721 <eyobmelkamu721@gmail.com>
2023-07-31 12:41:40 +0300

    Finish task 5: 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: pointer to the head node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head != NULL && *head != NULL)
+	{
+		tmp = *head;
+		*(head) = (*head)->next;
+		free(tmp);
+	}
+	/* For safety explicitly assign NULL to head */
+	*head = NULL;
+}
