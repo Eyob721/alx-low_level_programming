0777f1fbf4800b88c7af062e3d213f0bdd7122de
eyob721 <eyobmelkamu721@gmail.com>
2023-08-07 22:23:18 +0300

    Remove _putchar: not needed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,9 +9,9 @@
 #define RDWR_EOF 0
 #define RDWR_ERR -1
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 /* Library functions */
 int _strlen(char *s);
