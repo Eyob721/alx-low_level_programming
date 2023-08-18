22fb90cc693335b51c3ecb57ffafbb5c7a1d3ec3
eyob721 <eyobmelkamu721@gmail.com>
2023-08-18 10:20:06 +0300

    Fix minor mistake on strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	if (s1 != NULL && s2 != NULL)
-		while (*s1 != '\0' || *s2 != '\0')
+	{
+		while (*s1 != '\0' && *s2 != '\0')
 		{
 			if (*s1 != *s2)
 				return (*s1 - *s2);
-			s1++, s2++;
+			++s1, ++s2;
 		}
+	}
 	return (0);
 }
