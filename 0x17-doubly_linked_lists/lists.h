0bcede041983d1e9daa2e35a2b8b3b452a4feda7
eyob721 <eyobmelkamu721@gmail.com>
2024-01-25 13:09:13 +0300

    feat: start project (0x17. C - Doubly linked lists)

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Standard headers ----------------------------------------------------- {{{*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*}}}*/
+
+/* Data structures ------------------------------------------------------ {{{*/
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/*}}}*/
+
+#endif
